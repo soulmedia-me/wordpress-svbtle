fa481599f603a94c2a8b9ec97558fdf2b41f3d18
Joseph Scavone <josephscavone@gmail.com>
2012-04-07 09:27:58 -0500

    Updating code to match subtle changes

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </section>
-<header id="simple" class="bigsite"> 
+<header id="simple"> 
 	<h1><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
 	<h2><?php bloginfo( 'description' ); ?></h2>
 	<ul id="user_info">
@@ -7,12 +7,13 @@
 	</ul>
 <?php //get_sidebar(); ?>
 	<p class="networkify" style="color: #777">Original design by <a href="http://svbtle.com">Svbtle</a>.</p>
-	<figure id="logo_blog" class="">
+	<figure id="logo_blog">
 		<a href="/" style="background-image: url(http://scav1.com/wp-content/themes/obtvse/images/chain_link.png);">SCAVONE</a>
 		<span class="dot_pulse"></span>
 	</figure>
 </header>
-<aside id="network_blog" class="bigsite" style="display: block; ">
+
+<aside id="network_blog" class="start">
 	<a href="http://wordpress.org">
 		<h4>Wordpress</h4>
 	</a>
@@ -21,9 +22,11 @@
 	<h3 style="background-image: url(http://scav1.com/wp-content/themes/obtvse/images/chain_link_large.png);">&nbsp;</h3>
 	<p class="name"><?php bloginfo( 'name' ); ?></p>
 </div>
+
 <nav id="service_hook">
 	<a href="?" style="background-image: url(http://scav1.com/wp-content/themes/obtvse/images/bolt_large.png); background-size: 100%;">Service Name</a>
 </nav>
+
 <ul id="magic_bar">
 	<li class="triangle">&nbsp;</li>
 	<li class="catch"><p><?php bloginfo( 'name' ); ?></p></li>	
@@ -36,6 +39,7 @@ var custom_domain = "scav1.com";
 var appdomain = "scav1.com";
 var color = "66A3D2";
 </script>
+
 <style>
 a:hover, article h2 a:hover {color: #66A3D2;}
 a.buttonize:hover {background-color: #66A3D2;}
@@ -44,6 +48,7 @@ header#simple span.dot_pulse {border: 10px solid #66A3D2;}
 aside#network_blog.start {background-color: #66A3D2;}
 </style>
 
-<script src="http://scav1.com/wp-content/themes/obtvse/build.blog.js" type="text/javascript"></script>
+<!--<script src="http://scav1.com/wp-content/themes/obtvse/build.blog.js" type="text/javascript"></script>-->
+<script src="http://scav1.com/wp-content/themes/obtvse/s1.js" type="text/javascript"></script>
 </body>
 </html>
