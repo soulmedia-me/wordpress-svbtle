94be6746c25446281704a446cc50732c3d1e0dbb
Joseph Scavone <josephscavone@gmail.com>
2012-04-11 14:33:01 -0500

    updated kudos to account for new posts

--- kudos.php
+++ kudos.php
@@ -15,13 +15,17 @@ if (!$con)
 
 mysql_select_db($db, $con);
 
-$sql = "SELECT meta_value FROM wp_postmeta WHERE post_id = $external_id AND meta_key = 'wp-svbtle-kudos'";
+$sql = "SELECT IF(SUM(meta_value) IS NOT NULL, SUM(meta_value), 0) meta_value FROM wp_postmeta WHERE post_id = $external_id AND meta_key = 'wp-svbtle-kudos'";
 $result = mysql_query($sql,$con);
 $kudos =  mysql_result($result,0);
 
-$kudos = ($kudos + 1);
-
-mysql_query("UPDATE wp_postmeta SET meta_value = $kudos WHERE post_id = $external_id AND meta_key = 'wp-svbtle-kudos'");
+if ($kudos > "0") {
+	$kudos = ($kudos + 1);
+	mysql_query("UPDATE wp_postmeta SET meta_value = $kudos WHERE post_id = $external_id AND meta_key = 'wp-svbtle-kudos'");
+} else {
+	$kudos = ($kudos + 1);
+	mysql_query("INSERT INTO wp_postmeta (post_id, meta_key, meta_value) VALUES ($external_id, 'wp-svbtle-kudos', $kudos)");
+}
 
 mysql_close($con);
 
