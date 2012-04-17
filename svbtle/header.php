424ef992ae6e7f3f4ac08c5334dc9da814b35e50
Joseph Scavone <josephscavone@gmail.com>
2012-04-17 10:06:50 -0500

    Removed old ga script

--- svbtle/header.php
+++ svbtle/header.php
@@ -4,19 +4,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php bloginfo('name'); ?><?php wp_title('|',true); ?></title>
 	<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
-	<script type="text/javascript">
-		// ganalytics
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  		})();
- 		 var _gaq = _gaq || [];
-  		_gaq.push(['_setAccount', 'UA-30642855-1']);
-  		_gaq.push(['_setDomainName', 'scav1.com']);
-  		_gaq.push(['_setAllowLinker', true]);
-		_gaq.push(['_trackPageview']);
-	</script>
 	<style type="text/css">.tk-freight-sans-pro{font-family:"freight-sans-pro",sans-serif;}.tk-proxima-nova{font-family:"proxima-nova",sans-serif;}.tk-ff-meta-web-pro{font-family:"ff-meta-web-pro",sans-serif;}</style>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fonts.css">
 	<link href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="<?php _e('RSS'); ?>" type="application/rss+xml" />
