54714ba3d22eef29410dc8b290d7681a60da57cc
Joseph Scavone <josephscavone@gmail.com>
2012-04-16 21:22:24 -0500

    Removed unused <head> tags

--- svbtle/header.php
+++ svbtle/header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title><?php bloginfo('name'); ?><?php wp_title('|',true); ?></title>
 	<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
@@ -17,11 +17,8 @@
   		_gaq.push(['_setAllowLinker', true]);
 		_gaq.push(['_trackPageview']);
 	</script>
-	<meta property="og:type" content="website"/>
-	<meta property="og:url" content="<?php echo home_url(); ?>"/>
-	<meta property="og:type" content="website"/>
-	<meta property="og:description" content="<?php bloginfo( 'description' ); ?>"/>
-	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/svbtle_logo.png" /> 
+	<style type="text/css">.tk-freight-sans-pro{font-family:"freight-sans-pro",sans-serif;}.tk-proxima-nova{font-family:"proxima-nova",sans-serif;}.tk-ff-meta-web-pro{font-family:"ff-meta-web-pro",sans-serif;}</style>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fonts.css">
 	<link href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="<?php _e('RSS'); ?>" type="application/rss+xml" />
 	<meta name="Description" content="<?php bloginfo( 'description' ); ?>"/>
 	<?php add_action( 'wp_enqueue_scripts', 'main_css' );  ?>
