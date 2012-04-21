d853cebb92a40c819a81b9b5280ee08e25b2265c
Joseph Scavone <josephscavone@gmail.com>
2012-04-20 23:21:37 -0500

    Renamed blog.js to jquery.js

--- svbtle/footer.php
+++ svbtle/footer.php
@@ -25,7 +25,7 @@ function bootstrapUser(){
 	$.user.ccolor = "<?php echo substr_replace($options['color'], '', 0, 1); ?>";
 }
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/blog.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js" type="text/javascript"></script>
 
 </body>
 </html>
