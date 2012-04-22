280a2e5f6d777dc5a444543158a7496fff30f300
Joseph Scavone <josephscavone@gmail.com>
2012-04-22 16:15:43 -0500

    Updated site look

--- svbtle/footer.php
+++ svbtle/footer.php
@@ -1,22 +1,10 @@
 <?php $options = get_option ( 'svbtle_options' ); ?>
 </section>
-<style type="text/css">
-	a:hover { color: <?php echo $options['color']; ?>;}
-	article blockquote { border-color: <?php echo $options['color']; ?>;}
-	figure#user_logo div.pulse { border-color: <?php echo $options['color']; ?>; }
-	figure#user_logo div.logo, article a.kudos.completed div.circle div.filled{background-image: url(<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $options['icon']; ?>_large.png);}
-	figure#cover_logo{background-image: url(<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $options['icon']; ?>_large.png);}
-	nav.pagination span.next a, footer#paginate a.back_to_blog { border-color: <?php echo $options['color']; ?>; color: <?php echo $options['color']; ?>;}
-	nav.pagination span.next a:hover, nav.pagination span.prev a:hover, footer#paginate a.back_to_blog:hover {border-color:<?php echo $options['color']; ?>;background-color: <?php echo $options['color']; ?>;}
-	section#cover{background-color: <?php echo $options['color']; ?>;}
-	section#posts.preview header h2 {color: <?php echo $options['color']; ?>;}
-	div.pullback a {color: <?php echo $options['color']; ?>; border-color: <?php echo $options['color']; ?>;}
-	div.pullback a:hover {border-color: <?php echo $options['color']; ?>;background-color: <?php echo $options['color']; ?>;}
-	::-moz-selection { background: <?php echo $options['color']; ?>; color: #fff; text-shadow: none; }
-	::selection { background: <?php echo $options['color']; ?>; color: #fff; text-shadow: none; }
-</style>
+
+<script data-cfasync="false" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
 <script>
-function bootstrapUser(){
+function buildUser(){
 	$.user = {};
 	$.user.appdomain = "<?php echo substr_replace(home_url(), '', 0, 7); ?>";
 	$.user.cdomain = "<?php echo substr_replace(home_url(), '', 0, 7); ?>";
@@ -25,7 +13,7 @@ function bootstrapUser(){
 	$.user.ccolor = "<?php echo substr_replace($options['color'], '', 0, 1); ?>";
 }
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/blog.js" type="text/javascript"></script>
 
 </body>
 </html>
--- svbtle/header.php
+++ svbtle/header.php
@@ -3,22 +3,37 @@
 <head>
   <title> <?php bloginfo('name'); ?><?php wp_title('|',true); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <link href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="<?php _e('RSS'); ?>" type="application/rss+xml" />
   <meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
-  <meta name="generator" content="<?php echo substr_replace(home_url(), '', 0, 7); ?>" />
+	<style type="text/css">.tk-freight-sans-pro{font-family:"freight-sans-pro",sans-serif;}.tk-proxima-nova{font-family:"proxima-nova",sans-serif;}.tk-ff-meta-web-pro{font-family:"ff-meta-web-pro",sans-serif;}</style>
+	<link rel="stylesheet" href="http://scav1.com/wp-content/themes/svbtle/css/fonts.css">
+  <link href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="<?php _e('RSS'); ?>" type="application/rss+xml" />
 	<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-precomposed.png" />
   <?php add_action( 'wp_enqueue_scripts', 'main_css' );  ?>
   <?php wp_head(); ?>
 	<?php $options = get_option ( 'svbtle_options' ); ?>
   <?php echo $options['google_analytics'];?>
+
+	<style type="text/css">
+		a:hover { color: <?php echo $options['color']; ?>;}
+		nav.pagination span.next a, footer#paginate a.back_to_blog { border-color: <?php echo $options['color']; ?>; color: <?php echo $options['color']; ?>;}
+		section#posts.preview header h2 {color: <?php echo $options['color']; ?>;}		
+		div.pullback a {color: <?php echo $options['color']; ?>; border-color: <?php echo $options['color']; ?>;}
+		article blockquote, figure#user_logo div.pulse { border-color: <?php echo $options['color']; ?>;}
+		figure#user_logo div.logo, article a.kudos.completed div.circle div.filled, figure#cover_logo{background-image: url(<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $options['icon']; ?>_large.png);}
+		nav.pagination span.next a:hover, nav.pagination span.prev a:hover, footer#paginate a.back_to_blog:hover,  div.pullback a:hover{border-color:<?php echo $options['color']; ?>;background-color: <?php echo $options['color']; ?>;}
+		section#cover, figure#user_logo div.logo{background-color: <?php echo $options['color']; ?>;}
+		::-moz-selection,::selection { background: <?php echo $options['color']; ?>; color: #fff; text-shadow: none; }
+	</style>
 </head>
 <body>
-  <section id="cover" class="fixed">
+
+<section id="cover" class="fixed">
   <figure id="cover_logo">
     <div class="logo">&nbsp;</div>
   </figure>
   <h1 id="cover_user"><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
 </section>
+
 
 <section id="sidebar" class="fixed">
   <figure id="user_logo">
